0a22ed97765c4f8937abae863c36e094b2df217f
Yorhel <git@yorhel.nl>
2008-10-26 11:30:08 +0100

    Proper 'packaging' + readme + license file
    I've chosen a MIT license for this project, I'm pretty sure nobody
    would disagree with that.

--- examples/vndb.pl
+++ examples/vndb.pl
@@ -13,30 +13,28 @@ use warnings;
 use Cwd 'abs_path';
 our $ROOT;
 BEGIN {
-  ($ROOT = abs_path $0) =~ s{/util/vndb\.pl$}{};
+  ($ROOT = abs_path $0) =~ s{/examples/vndb\.pl$}{};
 }
 
+# make sure YAWF is findable
 use lib $ROOT.'/lib';
+# and make sure our own example modules are, too
+use lib $ROOT.'/examples';
+
+
 use YAWF;
 
 
-# default options
-our %O = (
+YAWF::init(
+
  # required
   namespace => 'VNDB',
   db_login => [ 'dbi:Pg:dbname=vndb', 'vndb', 'passwd' ],
+
  # optional
   debug => 1,
   logfile => $ROOT.'/data/logs/err.log',
-);
 
-
-# ...can be overwritten in data/config.pl
-require $ROOT.'/data/config.pl' if -e $ROOT.'/data/config.pl';
-
-
-YAWF::init(
-  %O,
   error_404_handler => \&page_404,
 );
 
